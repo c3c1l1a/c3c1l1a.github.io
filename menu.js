430174105d0343f089aff46a4ec2062003fcb199
Kweku Bright <kweku01bright@gmail.com>
2022-05-09 13:44:24 +0100

    Add click event to hamburger icon and close icon
    Add click event to hamburger icon and close icon

---
+++ menu.js
@@ -0,0 +1,13 @@
+//on-click on humburger menu display the menu
+let humburger = document.querySelector('.nav-icon');
+
+humburger.addEventListener('click', function () {
+  //show the mobile menu
+  document.querySelector('.menu').style.display = 'flex';
+});
+
+//hide the menu when close icon is clicked
+let close = document.querySelector('.close-icon');
+close.addEventListener('click', function () {
+  document.querySelector('.menu').style.display = 'none';
+});
