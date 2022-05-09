47359a7ee62ff260669e12c21986b105acf507ed
c3c1l1a <cesswangumukima@gmail.com>
2022-05-09 23:33:53 +0800

    Added the close icon and fixed a desktop menu bug

--- menu.js
+++ menu.js
@@ -3,21 +3,19 @@ let humburger = document.querySelector('.nav-icon');
 
 humburger.addEventListener('click', function () {
   //show the mobile menu
-  document.querySelector('.menu').style.display = 'flex';
+  document.querySelector('.mobile-menu').style.display = 'flex';
 });
 
 //hide the menu when close icon is clicked
 let close = document.querySelector('.close-icon');
 close.addEventListener('click', function () {
-  document.querySelector('.menu').style.display = 'none';
+  document.querySelector('.mobile-menu').style.display = 'none';
 });
 
 let navItems = document.querySelectorAll('.nav-item');
 navItems.forEach(function(navItem){
   navItem.addEventListener('click', function(){
-    document.querySelector('.menu').style.display = 'none';
-    console.log('what');
+    document.querySelector('.mobile-menu').style.display = 'none';
   }); 
 });
-   
 
