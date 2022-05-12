93d83e5859e58bb52d02846270f3d9cd58f1427f
c3c1l1a <cesswangumukima@gmail.com>
2022-05-13 01:30:18 +0800

    Fix spacing

--- contact-form.js
+++ contact-form.js
@@ -1,13 +1,12 @@
-const contactForm = document.querySelector(".js-contact-form");
-contactForm.addEventListener('submit', (event)=> {
-	event.preventDefault();
+const contactForm = document.querySelector('.js-contact-form');
+contactForm.addEventListener('submit', (event) => {
+  event.preventDefault();
 
-	const contactFormEmail = document.querySelector(".js-contact-form__email");
-	if (contactFormEmail.value !== contactFormEmail.value.toLowerCase()){
-		let error = 	document.querySelector(".js-input-error__email");
-		error.style.display = "block";
-	}
+  const contactFormEmail = document.querySelector('.js-contact-form__email');
+  if (contactFormEmail.value !== contactFormEmail.value.toLowerCase()) {
+    const error = document.querySelector('.js-input-error__email');
+    error.style.display = 'block';
+  }
 
-	event.target.reset();
+  event.target.reset();
 });
-
