7537bfba8a42f2166fcd3998805bada4e89e35dd
c3c1l1a <cesswangumukima@gmail.com>
2022-05-13 01:24:38 +0800

    Add style for input error

--- contact-form.js
+++ contact-form.js
@@ -1,12 +1,13 @@
-
 const contactForm = document.querySelector(".js-contact-form");
 contactForm.addEventListener('submit', (event)=> {
-	console.log("test");
 	event.preventDefault();
+
 	const contactFormEmail = document.querySelector(".js-contact-form__email");
 	if (contactFormEmail.value !== contactFormEmail.value.toLowerCase()){
-		let error = 	document.querySelector(".js-contact-form__error--emailCase");
+		let error = 	document.querySelector(".js-input-error__email");
 		error.style.display = "block";
 	}
+
+	event.target.reset();
 });
 
